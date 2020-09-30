0139c6cb2b09ce947c7a58c5b30dc0542686c3f5
IdanILT <idan@ilt.co.il>
2020-09-30 12:36:59 +0300

    rxjs

---
+++ packages/frontend/src/app.ts
@@ -0,0 +1,59 @@
+import {fromEvent} from "rxjs";
+import {filter, map, switchMap, tap} from "rxjs/operators";
+import { chatService } from './index'
+
+/// helpers
+const value = (id, value = undefined) => {
+    // @ts-ignore
+    const ret = document.getElementById(id).value || '';
+    if(value !== undefined){
+        // @ts-ignore
+        document.getElementById(id).value = value;
+    }
+    return ret;
+}
+/// events
+const create$ = fromEvent(document.getElementById('create'), 'click');
+const ch$ = fromEvent(document.getElementById('channel-list'), 'change');
+const msg$ = fromEvent(document.getElementById('message'), 'keypress').pipe(
+    filter( e => e.code === "Enter" )
+);
+
+/// epics
+create$
+    .pipe(
+        map(()=>value('new', '')),
+        tap( (v) => v && chatService.createChannel({topic: v}) )
+    )
+    .subscribe();
+
+ch$
+    .pipe(
+        tap(() => value('chat', '')),
+        switchMap(() => chatService.messages$({channel: value('channel-list')})),
+        tap(msg => value('chat', value('chat') + '\n' + msg.message))
+    )
+    .subscribe();
+
+msg$
+    .pipe(
+        map(() => ({ch: value('channel-list'), msg: value('message', '')})),
+        filter( i => i.ch && i.msg ),
+        tap(i => chatService.message({
+            header: {
+                channel: i.ch,
+                timestamp: Date.now(),
+                type: "text"
+            },
+            message: i.msg
+        }))
+    )
+    .subscribe();
+
+chatService
+    .channels$({})
+    .subscribe(
+        ch => document
+            .getElementById('channel-list')
+            .insertAdjacentHTML('beforeend', `<option value="${ch.id}">${ch.topic}</option>`)
+    )
--- packages/frontend/src/index.ts
+++ packages/frontend/src/index.ts
@@ -1,9 +1,29 @@
 import { createGatewayProxy } from "@scalecube/rsocket-ws-gateway/dist/createGatewayProxy";
 import {ChatServiceDefinition, ChatServiceAPI} from "@scalecube-chat-example/api";
+import {from} from "rxjs";
+import {switchMap} from "rxjs/operators";
 
-createGatewayProxy(
+export const chatServicePromise = createGatewayProxy(
     `ws://localhost:8000`,
     ChatServiceDefinition
 ).then((s:ChatServiceAPI.ChatService) => (window as any).ChatService = s);
 
-
+export const chatService = new Proxy(chatServicePromise, {
+   get(target: Promise<ChatServiceAPI.ChatService>, p: string): any {
+       if( !ChatServiceDefinition.methods[p] ) {
+           return;
+       }
+       switch (ChatServiceDefinition.methods[p].asyncModel) {
+           case "requestResponse":
+               return (...args) => new Promise((res, rej)=>{
+                   target
+                       .then( o => o[p](...args).then(res).catch(rej) )
+                       .catch(rej);
+               });
+           case "requestStream":
+               return (...args) => from(target).pipe(
+                   switchMap(svc => svc[p](...args))
+               )
+       }
+   }
+});
